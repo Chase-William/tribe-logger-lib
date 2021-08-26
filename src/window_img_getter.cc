2163d479a5e393cd51555a5d8db963147f151fe3
cclcr21 <cclcr21@rit.edu>
2021-08-26 11:47:50 -0400

    refactor for BITMAPINFO param in GetDIBits call

--- src/window_img_getter.cc
+++ src/window_img_getter.cc
@@ -61,57 +61,75 @@ WinImgRtrn GetNativeWindowBitmap(std::string windowName, unsigned long &size, bo
   // Get the BITMAP from the HBITMAP
   GetObject(hbmpTarget, sizeof(BITMAP), &bmpObj);
 
-  char* bmpBuffer = NULL;
+  long dpiX, dpiY;
+  // Get DPI of screen vertical / horizontal
+  HDC screen = GetDC(NULL);
+  dpiX = (GetDeviceCaps(screen, LOGPIXELSX));
+  dpiY = (GetDeviceCaps(screen, LOGPIXELSY));
+  ReleaseDC(NULL, screen);
 
-  if (includeFileHeader == true) { // Include the fileheader of 54 bytes at the header of the bitmap buffer
-    long dpiX, dpiY;
-    // Get DPI of screen vertical / horizontal
-    HDC screen = GetDC(NULL);
-    dpiX = (GetDeviceCaps(screen, LOGPIXELSX));
-    dpiY = (GetDeviceCaps(screen, LOGPIXELSY));
-    ReleaseDC(NULL, screen);
-
-    BITMAPFILEHEADER bmfHeader;
-    BITMAPINFOHEADER bi;
-    bi.biSize = sizeof(BITMAPINFOHEADER);
-    bi.biWidth = bmpObj.bmWidth;
-    bi.biHeight = bmpObj.bmHeight;
-    bi.biPlanes = 1;
-    bi.biBitCount = 32;
-    bi.biCompression = BI_RGB;
-    bi.biSizeImage = 0;
-    bi.biXPelsPerMeter = dpiX * INCH_TO_METER_RATIO;
-    bi.biYPelsPerMeter = dpiY * INCH_TO_METER_RATIO;
-    bi.biClrUsed = 0;
-    bi.biClrImportant = 0;
-    // aligning bits for words?
-    dwBmpSize = ((bmpObj.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpObj.bmHeight;
-    // Add the size of the headers to the size of the bmpBuffer to get the total file size.
+  BITMAPFILEHEADER bmfHeader;
+  BITMAPINFOHEADER bi;
+  bi.biSize = sizeof(BITMAPINFOHEADER);
+  bi.biWidth = bmpObj.bmWidth;
+  bi.biHeight = bmpObj.bmHeight;
+  bi.biPlanes = 1;
+  bi.biBitCount = 32;
+  bi.biCompression = BI_RGB;
+  bi.biSizeImage = 0;
+  bi.biXPelsPerMeter = dpiX * INCH_TO_METER_RATIO;
+  bi.biYPelsPerMeter = dpiY * INCH_TO_METER_RATIO;
+  bi.biClrUsed = 0;
+  bi.biClrImportant = 0;
+  // aligning bits for words?
+  dwBmpSize = ((bmpObj.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpObj.bmHeight;
+  // Add the size of the headers to the size of the bmpBuffer to get the total file size.
+  if (includeFileHeader) {
     dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-    size = dwSizeofDIB;  
-    // Offset to where the actual bmpBuffer bits start.
-    bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);    
-    bmpBuffer = (char*)malloc(dwSizeofDIB);
+    size = dwSizeofDIB;
+  } else {
+    size = dwBmpSize;  
+  }  
+    
+  char* bmpBuffer = (char*)malloc(dwSizeofDIB);
 
+  /*
+
+    IMPORTANT READ ---------------------------------
+
+
+    So the issue was that when calling GetDIBits you need a BITMAPINFO structure that contains meta data to be passed in with...
+    Should this be two seperate methods?
+    Figure out a better implementation that is clearer...
+  */
+
+
+  if (includeFileHeader) {
+    // Offset to where the actual bmpBuffer bits start.
+    bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);  
     // Get offset in bytes from file start
     DWORD pixelOffset = dwSizeofDIB - dwBmpSize;
     // Set pixel buffer pointer based on offset
     lpPixels = bmpBuffer + pixelOffset;
-    std::cout << "Sizeof-BITMAPFILEHEADER: " << sizeof(BITMAPFILEHEADER) << std::endl;
-    std::cout << "Sizeof-BITMAPINFOHEADER: " << sizeof(BITMAPINFOHEADER) << std::endl;
-    std::cout << "Offset-pixelOffset: " << pixelOffset << std::endl;
-    // Gets the "bits" from the bmpBuffer, and copies them into a buffer 
-    // that's pointed to by lpPixels.
-    GetDIBits(
-      hdcTarget, 
-      hbmpTarget, 
-      0,
-      (UINT)bmpObj.bmHeight,
-      lpPixels,
-      (BITMAPINFO*)&bi, 
-      DIB_RGB_COLORS
-    );
+  } else {
+    lpPixels = bmpBuffer;
+  }  
+  // std::cout << "Sizeof-BITMAPFILEHEADER: " << sizeof(BITMAPFILEHEADER) << std::endl;
+  // std::cout << "Sizeof-BITMAPINFOHEADER: " << sizeof(BITMAPINFOHEADER) << std::endl;
+  // std::cout << "Offset-pixelOffset: " << pixelOffset << std::endl;
+  // Gets the "bits" from the bmpBuffer, and copies them into a buffer 
+  // that's pointed to by lpPixels.
+  GetDIBits(
+    hdcTarget, 
+    hbmpTarget, 
+    0,
+    (UINT)bmpObj.bmHeight,
+    lpPixels,
+    (BITMAPINFO*)&bi, 
+    DIB_RGB_COLORS
+  );  
 
+  if (includeFileHeader) { // Copy over header info if desired
     // Size of the file.
     bmfHeader.bfSize = dwSizeofDIB;
     // bfType must always be BM for Bitmaps.
@@ -119,21 +137,7 @@ WinImgRtrn GetNativeWindowBitmap(std::string windowName, unsigned long &size, bo
     // Finish filling out buffer of entire bmpBuffer
     std::memcpy(bmpBuffer, &bmfHeader, sizeof(bmfHeader));
     std::memcpy(bmpBuffer +  sizeof(bmfHeader), &bi, sizeof(bi));
-  } else { // Get bitmap buffer without 54 byte header info
-    dwBmpSize = ((bmpObj.bmWidth * 32 + 31) / 32) * 4 * bmpObj.bmHeight;
-    size = dwBmpSize;
-    bmpBuffer = (char*)malloc(dwBmpSize);
-    BITMAPINFOHEADER bi;
-    GetDIBits(
-      hdcTarget, 
-      hbmpTarget, 
-      0,
-      (UINT)bmpObj.bmHeight,
-      bmpBuffer,
-      (BITMAPINFO*)&bi, // ----------------------------- check this if fails
-      DIB_RGB_COLORS
-    );
-  }
+  }  
 
 done:
   if (hbmpTarget) {
